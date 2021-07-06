304b5bf9c80efafc7cb9c00e154fe25beb0c3aa6
ahacad <ahacad@126.com>
2021-07-06 22:39:57 +0800

    feat: initial commit

---
+++ components/TOC.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { defineProps, ref } from "vue";
+
+const props = defineProps({
+  count: {
+    default: 0,
+  },
+});
+
+const counter = ref(props.count);
+
+const getClass = (index: number) => {
+  if (Number(counter.value) === 0) {
+    return "text-black";
+  }
+  if (index + 1 === Number(counter.value)) {
+    return "text-black";
+  } else {
+    return "text-gray-400";
+  }
+};
+
+const items = [
+  "Introduction to LSM-tree",
+  "Measuring Latency",
+  "Analyses on Merge Schedulers: Full Merges",
+  "Analyses on Merge Schedulers: Partitioned Merges",
+  "Lessons and Conclusions",
+  "References",
+];
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, key) in items" :key="key" :class="getClass(key)">
+        <a :href="$slidev.nav.currentPage">
+          {{ item }}
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
